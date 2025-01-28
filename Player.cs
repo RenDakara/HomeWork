1b93d3f3b936ed5119ece4928f02e6b12faa1caa
RenDakara <136173103+RenDakara@users.noreply.github.com>
2025-01-28 19:26:49 +0300

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -4,6 +4,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
     private Rigidbody _rigidbody;
+    private string _horizontal = "Horizontal";
+    private string _vertical = "Vertical";
 
     private void Awake()
     {
@@ -12,21 +14,11 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis(_horizontal);
+        float verticalInput = Input.GetAxis(_vertical);
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
 
-        if(movement.magnitude > 1)
-        {
-            movement.Normalize();
-        }
-
         _rigidbody.MovePosition(transform.position +  movement * _speed * Time.deltaTime);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(collision.collider.name);
     }
 }
