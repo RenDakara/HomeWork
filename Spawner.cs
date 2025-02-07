322d96c2ccbbbb98685e559524215d1dedfaa61c
RenDakara <136173103+RenDakara@users.noreply.github.com>
2025-02-07 22:14:17 +0300

    Add files via upload

---
+++ Spawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Coin _coin;
+    [SerializeField] private List<Transform> _coinSpawnPoint;
+
+
+    private void Start()
+    {
+       foreach (Transform spawnPoint in _coinSpawnPoint)
+        {
+            Instantiate(_coin, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
