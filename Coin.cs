e7bca94238e7a2e9d2458ec0cd4c0169b6fa0bf3
RenDakara <136173103+RenDakara@users.noreply.github.com>
2025-02-07 22:14:40 +0300

    Update Coin.cs

--- Coin.cs
+++ Coin.cs
@@ -1,14 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.TryGetComponent(out Player player))
-        {
-            Destroy(gameObject);
-        }
-    }
+   
 }
