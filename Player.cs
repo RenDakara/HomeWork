6ae4f7c3622de3d60e159bd64fa5f127ca961696
RenDakara <136173103+RenDakara@users.noreply.github.com>
2025-02-07 22:15:39 +0300

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -2,39 +2,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed = 3f;
-
     private bool _isRunning;
-    private SpriteRenderer _renderer;
     private Animator _animator;
+    private Movement _movement;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
-        _renderer = GetComponent<SpriteRenderer>();
+        _movement = GetComponent<Movement>();
     }
 
     private void Update()
+    {
+        Move();
+    }
+
+    private void FixedUpdate()
+    {
+        _movement.Jump();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Coin coin))
+        {
+            Destroy(coin.gameObject);
+        }
+    }
+
+    private void Move()
     {
         float moveInput = Input.GetAxis("Horizontal");
 
         if (moveInput != 0)
         {
             _isRunning = true;
-            Run();
+            _movement.Run();
         }
         else
         {
             _isRunning = false;
         }
 
-        _animator.SetBool("IsRunning", _isRunning);
-    }
-
-    private void Run()
-    {
-        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, _speed * Time.deltaTime);
-        _renderer.flipX = dir.x < 0.0f;
+        _animator.SetBool(PlayerAnimatorData.Params.IsRunning, _isRunning);
     }
 }
