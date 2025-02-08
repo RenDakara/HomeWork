84bcc81905b480b08d4bdca024d948e4313b7204
RenDakara <136173103+RenDakara@users.noreply.github.com>
2025-02-08 20:52:29 +0300

    Update Spawner.cs

--- Spawner.cs
+++ Spawner.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Coin _coin;
-    [SerializeField] private List<Transform> _coinSpawnPoint;
+    [SerializeField] private List<Transform> _coinSpawnPosition;
 
     private void Start()
     {
-       foreach (Transform spawnPoint in _coinSpawnPoint)
+       foreach (Transform spawnPoint in _coinSpawnPosition)
         {
             Instantiate(_coin, spawnPoint.position, spawnPoint.rotation);
         }
