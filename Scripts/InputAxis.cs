f8b2ac6e0796b9f4076bf99152f87956180121fa
RenDakara <136173103+RenDakara@users.noreply.github.com>
2025-02-14 14:20:09 +0300

    Delete Scripts directory

--- Scripts/InputAxis.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class InputAxis 
-{
-    public const string Horizontal = "Horizontal";
-    public const string Jump = "Jump";
-}
