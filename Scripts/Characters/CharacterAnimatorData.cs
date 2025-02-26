19ab83d6edc793ee2bd16b12305bf8c0ec05e91a
RenDakara <136173103+RenDakara@users.noreply.github.com>
2025-02-26 14:49:07 +0300

    Delete Scripts directory

--- Scripts/Characters/CharacterAnimatorData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public static class CharacterAnimatorData
-{
-  public static class Params
-    {
-        public static readonly int IsRunning = Animator.StringToHash(nameof(IsRunning));
-    }
-}
